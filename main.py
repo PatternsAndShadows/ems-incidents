371461cf8c569ba905f7715a865d46739857cecd
PatternsAndShadows <jason@3rdeast.com>
2026-06-16 17:38:39 -0600

    update logging

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def main(args):
     transform = t.IndianaEmsTransform(args)
     loader = l.LoadEmsData(args)
 
-    source_dataset = '42c5f7f2-bc83-4c97-a5bf-787fdfc8e667'
+    source_dataset = '42c5f7f2-bc83-4c97-a5bf-787fdfc8e667'         # TODO add to property file
     transform.set_df(extract.extract_sourcedata(source_dataset))
     transform.transform_ems_incident_data()                        #TODO create a factory class?
     loader.set_df(transform.get_transformed_df())
